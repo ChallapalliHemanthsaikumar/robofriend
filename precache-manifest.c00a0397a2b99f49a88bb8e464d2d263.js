f9414ea7ed5b4a4e19a78f738b36802c54cebd11
ChallapalliHemanthsaikumar <67137724+ChallapalliHemanthsaikumar@users.noreply.github.com>
2021-11-30 22:31:17 +0530

    Updates

---
+++ precache-manifest.c00a0397a2b99f49a88bb8e464d2d263.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "063d211da6df1918341c1de0879ffef9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "813ebe233f86ae990ea1",
+    "url": "./static/css/2.d755d3a8.chunk.css"
+  },
+  {
+    "revision": "fbc2e69e9647b5afec22",
+    "url": "./static/css/main.d5c0bc01.chunk.css"
+  },
+  {
+    "revision": "813ebe233f86ae990ea1",
+    "url": "./static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbc2e69e9647b5afec22",
+    "url": "./static/js/main.52679944.chunk.js"
+  },
+  {
+    "revision": "3af9496b79045c74a07c",
+    "url": "./static/js/runtime-main.bddfdab7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "./static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriend/precache-manifest.186e64052005f0d06162eab57baaaa99.js"
+  "./precache-manifest.c00a0397a2b99f49a88bb8e464d2d263.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
